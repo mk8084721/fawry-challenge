b10ce67586a34c1a50626ffdff7765f7beb25e52
mk8084721 <73140794+mk8084721@users.noreply.github.com>
2025-07-05 19:40:59 +0300

    implementing regular product
    Regular product (non-expirable, non-shippable)

---
+++ src/main/java/org/historicaltrash/model/RegularProduct.java
@@ -0,0 +1,7 @@
+package org.historicaltrash.model;
+
+public class RegularProduct extends Product {
+    public RegularProduct(String name, double price, int quantity) {
+        super(name, price, quantity);
+    }
+}
