6430ccce5221695987821690bdfaaa7095d74a95
mk8084721 <73140794+mk8084721@users.noreply.github.com>
2025-07-05 23:06:24 +0300

    clean the main

--- src/main/java/org/historicaltrash/Main.java
+++ src/main/java/org/historicaltrash/Main.java
@@ -9,25 +9,6 @@ import java.time.LocalDate;
 import java.util.List;
 
 public class Main {
-    /*public static void main(String[] args) {
-        Product product = new RegularProduct("regular Product",3.00,5);
-
-        Product expiredProduct = new ExpirableProduct("Cheese",3.00,5, LocalDate.of(2026,3,2));
-        System.out.println(expiredProduct instanceof Expirable ? ((ExpirableProduct) expiredProduct).getExpiryDate() : "not Expirable");
-
-        Product shippableProduct = new ShippableProduct("Car",3.00,5, 44.00);
-        System.out.println(shippableProduct instanceof Shippable ? ((Shippable) shippableProduct).getWeight() : "not Shippable");
-
-        ExpirableShippableProduct expirableShippableProduct = new ExpirableShippableProduct("Fridge",3.00,5,
-                LocalDate.of(2026,3,2), 55.0);
-        System.out.println((expirableShippableProduct instanceof Shippable && expirableShippableProduct instanceof Expirable)?
-                ((ExpirableShippableProduct) expirableShippableProduct).getWeight()+"\n"+
-                        ((ExpirableShippableProduct) expirableShippableProduct).getExpiryDate() : "not ExpirableShippableProduct");
-
-        ShippingService shippingService = new ShippingService();
-        //shippingService.shipItems(List.of(expirableShippableProduct,shippableProduct));
-
-    }*/
     public static void main(String[] args) {
         // Create products
         Product tv = new ShippableProduct("TV", 599.99, 10, 15.5);
